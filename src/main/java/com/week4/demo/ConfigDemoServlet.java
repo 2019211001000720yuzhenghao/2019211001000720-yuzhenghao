4cf60e08f8cf61044f8b9770859f92665aa5b5ae
2019211001000720-yuzhenghao <2482007459@qq.com>
2021-06-08 09:51:44 +0800

    HW lab2 commit

--- src/main/java/com/week4/demo/ConfigDemoServlet.java
+++ src/main/java/com/week4/demo/ConfigDemoServlet.java
@@ -6,24 +6,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-@WebServlet(
-        urlPatterns = {"/config"},
-        initParams = {
-                @WebInitParam(name="name",value = "YuZhengHao"),
-                @WebInitParam(name="studentId",value = "2019211001000720"),
-        },loadOnStartup = 1
-)
+//@WebServlet(
+////        method 1
+//        urlPatterns = {"/config"},
+//        initParams = {
+//                @WebInitParam(name="id",value = "2019211001000720"),
+//                @WebInitParam(name="name",value = "JiangTao"),
+//        },loadOnStartup = 1
+//)
+
 public class ConfigDemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletConfig config = getServletConfig();
-        String name = config.getInitParameter("name");
-        String id = config.getInitParameter("studentId");
-        PrintWriter writer = response.getWriter();
-        writer.println("Name:" + name);
-        writer.println("studentId:" + id);
-    }
+        ServletConfig config=getServletConfig();
+        String id=config.getInitParameter("id");
+        String name=config.getInitParameter("name");
 
+        PrintWriter writer= response.getWriter();
+        writer.println("studentId: "+id);
+        writer.println("name: "+name);
+    }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
